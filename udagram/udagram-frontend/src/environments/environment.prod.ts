615d5c2f8868c05f97d8b4a02d926e2a49ffa4e7
LamLe <vanlam1503@gmail.com>
2024-11-28 14:16:21 +0700

    Change url front end

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-env01.eba-9uzpehmc.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'https://udagram-api-env01.eba-w7q42zjz.us-east-1.elasticbeanstalk.com/api/v0'
 };
 /*
  * For easier debugging in development mode, you can import the following file
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'https://udagram-api-env01.eba-9uzpehmc.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'https://udagram-api-env01.eba-w7q42zjz.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
